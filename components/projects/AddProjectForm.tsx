805cb08cfdb2f7ad8f736cb0f871569dac25c88a
soknanprogramming <poch.soknan@gmail.com>
2026-03-17 22:02:40 +0700

    add refresh page

--- components/projects/AddProjectForm.tsx
+++ components/projects/AddProjectForm.tsx
@@ -2,17 +2,24 @@
 import { useEffect } from "react";
 import { useActionState } from "react";
 import { addProject } from "@/actions/project/addProject";
+import { useRouter } from "next/navigation";
+
 
 const initialState = { success: false, message: "" };
 
 export default function AddProjectForm() {
   const [state, formAction] = useActionState(addProject, initialState);
+  const router = useRouter();
+
 
   useEffect(() => {
     if (state.message) {
       alert(state.message);
+      if (state.success) {
+        router.refresh();
+      }
     }
-  }, [state.message]);
+  }, [state.message, state.success, router]);
 
   return (
     <div className="pt-2">
