48e2fb0b6972d67deac2d4e45e1d794b04cf622c
soknanprogramming <poch.soknan@gmail.com>
2026-03-21 12:11:06 +0700

    I has update some error

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,18 +3,22 @@
 import { register } from "@/actions/register/register";
 import { useActionState, useEffect } from "react";
 import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 const RegisterPage = () => {
   const [state, formAction] = useActionState(register, null);
+  const router = useRouter();
 
   useEffect(() => {
     if (state?.message && state?.success === true) {
       if (state.success) {
         redirect("/signin");
+      } else {
+        router.refresh();
+        alert(state.message);
       }
-      alert(state.message);
     }
-  }, [state]);
+  }, [state, router]);
 
   return (
     <div className="space-y-2">
@@ -28,11 +32,21 @@ const RegisterPage = () => {
       <form action={formAction} className="flex flex-col gap-2 w-70">
         <div className="flex flex-col">
           <label htmlFor="username">Username</label>
-          <input className="border p-1 ml-2 rounded-sm" type="tel" name="username" id="username" />
+          <input
+            className="border p-1 ml-2 rounded-sm"
+            type="tel"
+            name="username"
+            id="username"
+          />
         </div>
         <div className="flex flex-col">
           <label htmlFor="email">Email</label>
-          <input className="border ml-2 p-1 rounded-sm" type="email" name="email" id="email" />
+          <input
+            className="border ml-2 p-1 rounded-sm"
+            type="email"
+            name="email"
+            id="email"
+          />
         </div>
         <div className="flex flex-col">
           <label htmlFor="password">Password</label>
@@ -44,7 +58,10 @@ const RegisterPage = () => {
           />
         </div>
         <div>
-          <button className="dark:bg-gray-800 dark:hover:bg-gray-700 bg-gray-400 hover:bg-gray-5 hover:cursor-pointer p-1 rounded-sm" type="submit">
+          <button
+            className="dark:bg-gray-800 dark:hover:bg-gray-700 bg-gray-400 hover:bg-gray-5 hover:cursor-pointer p-1 rounded-sm"
+            type="submit"
+          >
             Register
           </button>
         </div>
